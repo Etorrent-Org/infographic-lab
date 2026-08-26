6b0988202416344844b760563e836a799eabae9a
Erwann Torrent <279009398+R1-Torrent@users.noreply.github.com>
2026-08-26 09:36:52 +0200

    Sentimental - branche le moteur premium sans modifier le studio

---
+++ src/marketing-wow.ts
@@ -0,0 +1,19 @@
+export {
+  campaignCopyMarkdown,
+  defaultMarketingCampaign,
+  marketingFormats,
+  mockupOptions,
+  objectiveOptions,
+  svgStringToDataUrl,
+  svgStringToRaster,
+  templateOptions,
+  toneOptions,
+} from "./marketing";
+
+export type {
+  MarketingCampaign,
+  MarketingFormatId,
+  MarketingMockup,
+} from "./marketing";
+
+export { renderMarketingSvg } from "./marketing-wow-renderer";
