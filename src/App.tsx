a0a16ad4b171cdf6dcd211b224deb3dcbd4db8d4
Erwann Torrent <279009398+R1-Torrent@users.noreply.github.com>
2026-08-27 11:54:22 +0200

    UI - wire compact preview modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AugmentedStudioV2 } from "./AugmentedStudio.v2";
 import { GenerationPreferencesBar } from "./GenerationPreferencesBar";
+import { PreviewModalEnhancement } from "./PreviewModalEnhancement";
 
 export function App() {
   return (
     <>
       <AugmentedStudioV2 />
+      <PreviewModalEnhancement />
       <GenerationPreferencesBar />
     </>
   );
